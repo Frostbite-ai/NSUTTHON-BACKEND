236076b09fe0b8ddb1c4a4e738c73a571d871e23
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-08-16 16:14:54 +0530

    initial

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+require("dotenv").config({ path: "./.env" });
+console.log(process.env.DATABASE_URL);
+
+module.exports = {
+  development: {
+    client: "pg",
+
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // Include this line for SSL configuration
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  // Add other environments like 'production', 'staging', etc. as needed
+};
