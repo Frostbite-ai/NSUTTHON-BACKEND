3a2b397d3bdadb4712c1c5302bbdcf9cc3be26b3
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-10-02 22:02:35 +0530

    github action

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,20 @@ module.exports = {
       tableName: "knex_migrations",
     },
   },
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: true }, // Set to true in production
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 
   // Add other environments like 'production', 'staging', etc. as needed
 };
